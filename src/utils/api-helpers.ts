1fe2c85adc9cbb5eef26255d10058c52f35bcaaf
ryomaD1225 <ryoma20151225@gmail.com>
2022-01-19 20:39:44 +0900

    axiosで対応

--- src/utils/api-helpers.ts
+++ src/utils/api-helpers.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export async function fetchGetJSON(url: string) {
   try {
     const data = await fetch(url).then((res) => res.json());
@@ -9,21 +11,38 @@ export async function fetchGetJSON(url: string) {
 
 export async function fetchPostJSON(url: string, data?: {}) {
   try {
+    const response = await axios.post(
+      `https://stupefied-feynman-e5c559.netlify.app/.netlify/functions/charge`,
+      {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data || {}), // body data type must match "Content-Type" header
+      }
+    );
+    return response;
     // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *client
-      body: JSON.stringify(data || {}), // body data type must match "Content-Type" header
-    });
-    return await response.json(); // parses JSON response into native JavaScript objects
+    // const response = await fetch(url, {
+    //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    //   mode: 'cors', // no-cors, *cors, same-origin
+    //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //   credentials: 'same-origin', // include, *same-origin, omit
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     // 'Content-Type': 'application/x-www-form-urlencoded',
+    //   },
+    //   redirect: 'follow', // manual, *follow, error
+    //   referrerPolicy: 'no-referrer', // no-referrer, *client
+    //   body: JSON.stringify(data || {}), // body data type must match "Content-Type" header
+    // });
+    // return await response.json(); // parses JSON response into native JavaScript objects
   } catch (err: any) {
     throw new Error(err.message);
   }
